b72f91c7c3c560885dddbf4b382b6969a7ee9174
Jun4928 <jun.choi.4928@gmail.com>
2021-07-11 00:35:53 +0900

    Add: reduce

--- stream.exs
+++ stream.exs
@@ -7,22 +7,24 @@ defmodule Benchmark do
   end
 end
 
-double = &(&1 * 2)
-is_even = &(rem(&1, 2) == 0)
+double = fn (n) -> n * 2 end
+is_even = fn (n) -> rem(n, 2) == 0 end
+add = fn (a, b) -> a + b end
 
 using_enum = fn range ->
     range
     |> Enum.filter(is_even) 
     |> Enum.map(double)
-    |> IO.inspect
+    |> Enum.reduce(add)
+    |> IO.puts
   end
 
 using_stream = fn range ->
     range
     |> Stream.filter(is_even)
     |> Stream.map(double) 
-    |> Enum.to_list()
-    |> IO.inspect
+    |> Enum.reduce(add)
+    |> IO.puts
   end
 
 using_enum.(1..100)
